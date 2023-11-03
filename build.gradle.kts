89e7f7acf00257d2949645f7be8546cfd1843598
Clara V. Alarcon <claravictorialarcon@gmail.com>
2023-11-03 00:38:54 -0300

    feat(mobile): adding new dependencies needed and modifying version of java and other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@
 
 plugins {
     id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.8.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false //Might  be 1.8.20 instead of 1.9.0
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
+    //id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+
 }
