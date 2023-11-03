4c8753c14457b58256a61a34077d68c3bd01fddf
Clara V. Alarcon <claravictorialarcon@gmail.com>
2023-11-03 00:33:55 -0300

    fix(mobile): deleted unnecesary api key and modifying core files and application file

--- app/src/main/java/com/example/ohmybenefits/core/InterceptorCustom.kt
+++ app/src/main/java/com/example/ohmybenefits/core/InterceptorCustom.kt
@@ -4,12 +4,10 @@ import okhttp3.Interceptor
 import okhttp3.Response
 object InterceptorCustom : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val apiKey = Config.apiKey
-
 
         var request = chain.request()
+
         request = request.newBuilder()
-            .header("X-Api-Key", apiKey?:"")
             .header("Accept", "application/json")
             .header("Content-Type", "application/json")
             .build()
