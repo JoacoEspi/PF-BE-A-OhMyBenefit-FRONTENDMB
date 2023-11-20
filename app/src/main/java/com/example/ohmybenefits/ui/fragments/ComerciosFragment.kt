8dc8fd22ef68564b42870965a408d2f2fe8e3c7c
Joaquin Espiñeira <joaespi@hotmail.com>
2023-11-20 17:21:07 -0300

    build(frontendmb): geolocation xml location button created

--- app/src/main/java/com/example/ohmybenefits/ui/fragments/ComerciosFragment.kt
+++ app/src/main/java/com/example/ohmybenefits/ui/fragments/ComerciosFragment.kt
@@ -25,7 +25,20 @@ class ComerciosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentComerciosBinding.inflate(inflater, container, false)
-        comerciosViewModel.obtenerComercios(-34.5270621941584, -58.49915655584191, 2, "")
+
+        //comerciosViewModel.obtenerComercios(-34.5270621941584, -58.49915655584191, 2, "")
+
+        binding.btnObtenerUbicacion.setOnClickListener {
+            // Lógica para obtener ubicación
+            obtenerUbicacion()
+        }
+
+        binding.btnEnviar.setOnClickListener {
+            // Lógica para enviar la dirección
+            val direccion = binding.edtDireccion.text.toString()
+            obtenerComercios(null, null, 2, direccion)
+        }
+
         comerciosViewModel.comerciosResponse.observe(viewLifecycleOwner, Observer { response ->
             Log.d("ComerciosFragment", "Callback ejecutado")
             if (response.isSuccessful) {
@@ -40,11 +53,25 @@ class ComerciosFragment : Fragment() {
                 Log.e("ComerciosFragment", "Error: $errorBody")
             }
         })
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun obtenerUbicacion() {
+        // Lógica para obtener ubicación del dispositivo
+        // ...
+        val latitud = -34.5270621941584
+        val longitud = -58.49915655584191
+        // Luego, llama a obtenerComercios con las coordenadas obtenidas
+        obtenerComercios(latitud, longitud, 2, "")
+    }
+
+    private fun obtenerComercios(latitud: Double?, longitud: Double?, codigoComercio: Int, direccion: String?) {
+        comerciosViewModel.obtenerComercios(latitud, longitud, codigoComercio, direccion)
     }
 }
